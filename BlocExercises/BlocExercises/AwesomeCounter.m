20451ac046ac4c31a49d0004df4347eca7c4e1fc
Donald Dixon <dixondonald@gmail.com>
2016-01-19 15:53:21 -0500

    Assignment 12 completed

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,26 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+
+    NSMutableString *numberString = [[NSMutableString alloc] init];
+    NSInteger lowNumber;
+    NSInteger highNumber;
+    
+    if (number <= otherNumber) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    } else {
+        lowNumber = otherNumber;
+        highNumber = number;
+    }
+    
+    while (lowNumber <= highNumber) {
+        [numberString appendFormat:@"%ld", lowNumber];
+        ++lowNumber;
+    }
+    
+    
+ return numberString;
 }
 
 @end
